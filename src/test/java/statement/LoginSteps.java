09a54472680a2bc7809dc55cfa57cc135d86c29f
SONANATARAJAN <sonanatarajan02@gmail.com>
2026-01-17 19:03:50 +0530

    Final Working Code

--- src/test/java/statement/LoginSteps.java
+++ src/test/java/statement/LoginSteps.java
@@ -1,11 +1,22 @@
 package statement;
 
-import io.cucumber.java.en.Given;
+import io.cucumber.java.en.*;
+import org.openqa.selenium.By;
 
 public class LoginSteps {
 
-    @Given("user is on login page")
-    public void user_is_on_login_page() {
-        System.out.println("🔥 STEP EXECUTED 🔥");
+    @Given("user opens browser")
+    public void open_browser() {
+        Hooks.driver.get("https://example.com");
+    }
+
+    @When("user enters credentials")
+    public void enter_credentials() {
+        System.out.println("Entering credentials");
+    }
+
+    @Then("login should be successful")
+    public void login_success() {
+        System.out.println("Login success");
     }
 }
--- src/test/java/statement/RunnerTest.java
+++ src/test/java/statement/RunnerTest.java
@@ -2,23 +2,21 @@ package statement;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 @CucumberOptions(
         features = "src/test/resources/feature",
         glue = "statement",
         plugin = {
                 "pretty",
-                "html:target/cucumber-report.html",
-                "json:target/cucumber-report.json"
+                "html:target/cucumber-report.html"
         },
         monochrome = true
 )
 public class RunnerTest extends AbstractTestNGCucumberTests {
 
-    @Override
-    @DataProvider
-    public Object[][] scenarios() {
-        return super.scenarios();
+    @Test
+    public void runCucumber() {
+        // DO NOT REMOVE – THIS TRIGGERS TESTNG
     }
 }
